11bf02c1196259ae20f99fb788b7ac4e2a52cf56
Ruan Souza <ruan2014souza17@gmail.com>
2023-09-15 10:38:32 -0300

    content criado e todo projeto dinamizado

---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Novo Filme do Capitão Angular",
+        "description":"Studios anuncia novo Filme",
+        "photoCover": "https://hermes.dio.me/articles/cover/63806b6d-b23f-4b34-b6ad-2b2345dae8c0.png",
+    },
+
+    {
+        "id":"2",
+        "title":"Filme da Historia da Marvel Anunciado",
+        "description":"Filme com previsão para sair em 2025...",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyQ0QdiTnGxTZsg80c1y24amxSHv2LKPHtuA&usqp=CAU",
+    }
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,12 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  
+ 
+
   constructor(){}
   
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+  ngOnInit(): void {}
 
 
 
